f4d1f1454d40ef9e86f88634037583a9073aa9ae
Xiaojing Zhang <80235074+zhangxjohn@users.noreply.github.com>
2021-11-23 19:22:25 +0800

    Init constants.

---
+++ hyperts/utils/consts.py
@@ -0,0 +1,15 @@
+# -*- coding:utf-8 -*-
+
+
+TIMESTAMP               = "timestamp"
+
+
+TASK_FORECAST           = 'forecast'
+TASK_BINARY             = 'binary'
+TASK_MULTICLASS         = 'multiclass'
+TASK_REGRESSION         = 'regression'
+
+
+DATATYPE_INT            = 'int'
+DATATYPE_FLOAT          = 'float'
+DATATYPE_OBJECT         = 'object'
