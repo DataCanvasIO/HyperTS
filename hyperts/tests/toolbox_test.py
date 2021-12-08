2a10bc71011ec8d64963a739ae5622129c0cfa84
Xiaojing Zhang <80235074+zhangxjohn@users.noreply.github.com>
2021-12-08 14:30:48 +0800

    Build the TsToolBox framework.

--- hyperts/tests/toolbox_test.py
+++ hyperts/tests/toolbox_test.py
@@ -5,7 +5,7 @@ import hyperts.datasets.base as ds
 
 def testimporttoolbox():
     print("test testImportToolBox")
-    X, y = ds.load_random_univariate_forecast_dataset()
+    X, y = ds.load_random_univariate_forecast_dataset(return_X_y=True)
     X_dask = dd.from_pandas(X.head((10)), 1)
     tstb = tb.get_tool_box(X_dask)
     print(tstb)
