d78387567ca2eb287630a70067041fd59cdc4ecb
Xiaojing Zhang <80235074+zhangxjohn@users.noreply.github.com>
2021-12-17 16:18:04 +0800

    Supported for rich metrics, including string and functional form (#25).

--- hyperts/utils/metrics.py
+++ hyperts/utils/metrics.py
@@ -1,5 +1,32 @@
 import numpy as np
 from sklearn.metrics import *
+from hypernets.utils import const
+
+greater_is_better = {
+    'mse': False,
+    'mae': False,
+    'rmse': False,
+    'mape': False,
+    'smape': False,
+    'r2_score': True,
+    'explained_variance_score': True,
+    'max_error': False,
+    'mean_absolute_error': False,
+    'mean_squared_error': False,
+    'mean_squared_log_error': False,
+    'median_absolute_error': False,
+    'mean_absolute_percentage_error': False,
+    'mean_pinball_loss': False,
+    'mean_tweedie_deviance': False,
+    'mean_poisson_deviance': False,
+    'mean_gamma_deviance': False,
+
+    'accuracy_score': True,
+    'balanced_accuracy_score': True,
+    'top_k_accuracy': True,
+    'roc_auc': True,
+    # ...
+}
 
 def check_is_array(y_true, y_pred):
     """Check whether the value is array-like.
@@ -9,6 +36,13 @@ def check_is_array(y_true, y_pred):
         y_true = np.array(y_true)
     if not isinstance(y_pred, np.ndarray):
         y_pred = np.array(y_pred)
+
+    if y_true.ndim == 1:
+        y_true = y_true.reshape((-1, 1))
+
+    if y_pred.ndim == 1:
+        y_pred = y_pred.reshape((-1, 1))
+
     return y_true, y_pred
 
 
@@ -134,4 +168,119 @@ def smape(y_true, y_pred, axis=None):
     return 2.0 * diff
 
 
+metric2scoring = {
+    'auc': 'roc_auc_ovo',
+    'accuracy': 'accuracy',
+    'accuracy_score': 'accuracy',
+    'recall': 'recall',
+    'recall_score': 'recall',
+    'precision': 'precision',
+    'precision_score': 'precision',
+    'f1': 'f1',
+    'f1_score': 'f1',
+    'mse': 'neg_mean_squared_error',
+    'neg_mean_squared_error': 'neg_mean_squared_error',
+    'mean_squared_error': 'neg_mean_squared_error',
+    'mae': 'neg_mean_absolute_error',
+    'neg_mean_absolute_error': 'neg_mean_absolute_error',
+    'mean_absolute_error': 'neg_mean_absolute_error',
+    'msle': 'neg_mean_squared_log_error',
+    'neg_mean_squared_log_error': 'neg_mean_squared_log_error',
+    'mean_squared_log_error': 'neg_mean_squared_log_error',
+    'rmse': 'neg_root_mean_squared_error',
+    'neg_root_mean_squared_error': 'neg_root_mean_squared_error',
+    'root_mean_squared_error': 'neg_root_mean_squared_error',
+    'r2': 'r2',
+    'r2_score': 'r2',
+    'logloss': 'neg_log_loss',
+    'log_loss': 'neg_log_loss',
+    'mape': mape,
+    'smape': smape
+    # ...
+}
 
+def metric_to_scorer(metric, task, pos_label=None, **options):
+    if pos_label is not None:
+        options['pos_label'] = pos_label
+    if 'binaryclass' in task:
+        options['average'] = 'binary'
+    elif 'multiclass' in task:
+        options['average'] = 'macro'
+    if isinstance(metric, str) and isinstance(metric2scoring[metric], str):
+        return get_scorer(metric2scoring[metric])
+    elif isinstance(metric, str) and callable(metric2scoring[metric]):
+        options.update({'greater_is_better': greater_is_better[metric]})
+        return make_scorer(metric2scoring[metric], **options)
+    elif metric.__name__ in metric2scoring.keys():
+        if isinstance(metric2scoring[metric.__name__], str):
+            return get_scorer(metric2scoring[metric.__name__])
+        else:
+            options.update({'greater_is_better': greater_is_better[metric.__name__]})
+            return make_scorer(metric2scoring[metric.__name__], **options)
+    elif options.get('greater_is_better') is not None:
+        return make_scorer(metric, **options)
+    else:
+        raise ValueError('Note that greater_is_better(type:Bool) need be provided.')
+
+
+def calc_score(y_true, y_preds, y_proba=None, metrics=('accuracy',), task=const.TASK_BINARY, pos_label=1,
+               classes=None, average=None):
+    score = {}
+    if y_proba is None:
+        y_proba = y_preds
+
+    if average is None:
+        if task == const.TASK_MULTICLASS:
+            average = 'macro'
+        else:
+            average = 'binary'
+
+    recall_options = dict(average=average, labels=classes)
+    if pos_label is not None:
+        recall_options['pos_label'] = pos_label
+
+    for metric in metrics:
+        if callable(metric):
+            try:
+                score[metric.__name__] = metric(y_true, y_preds)
+            except:
+                score[metric.__name__] = metric(y_true, y_preds, **recall_options)
+        else:
+            metric_lower = metric.lower()
+            if metric_lower == 'auc':
+                if len(y_proba.shape) == 2:
+                    if task == const.TASK_MULTICLASS:
+                        score[metric] = roc_auc_score(y_true, y_proba, multi_class='ovo', labels=classes)
+                    else:
+                        score[metric] = roc_auc_score(y_true, y_proba[:, 1])
+                else:
+                    score[metric] = roc_auc_score(y_true, y_proba)
+            elif metric_lower == 'accuracy':
+                if y_preds is None:
+                    score[metric] = 0
+                else:
+                    score[metric] = accuracy_score(y_true, y_preds)
+            elif metric_lower in ['recall']:
+                score[metric] = recall_score(y_true, y_preds, **recall_options)
+            elif metric_lower in ['precision']:
+                score[metric] = precision_score(y_true, y_preds, **recall_options)
+            elif metric_lower in ['f1']:
+                score[metric] = f1_score(y_true, y_preds, **recall_options)
+            elif metric_lower in ['mse', 'mean_squared_error', 'neg_mean_squared_error']:
+                score[metric] = mse(y_true, y_preds)
+            elif metric_lower in ['mae', 'mean_absolute_error', 'neg_mean_absolute_error']:
+                score[metric] = mae(y_true, y_preds)
+            elif metric_lower in ['msle', 'mean_squared_log_error', 'neg_mean_squared_log_error']:
+                score[metric] = mean_squared_log_error(y_true, y_preds)
+            elif metric_lower in ['rmse', 'root_mean_squared_error', 'neg_root_mean_squared_error']:
+                score[metric] = rmse(y_true, y_preds)
+            elif metric_lower in ['mape']:
+                score[metric] = mape(y_true, y_preds)
+            elif metric_lower in ['smape']:
+                score[metric] = smape(y_true, y_preds)
+            elif metric_lower in ['r2', 'r2_score']:
+                score[metric] = r2_score(y_true, y_preds)
+            elif metric_lower in ['logloss', 'log_loss']:
+                score[metric] = log_loss(y_true, y_proba, labels=classes)
+
+    return score
