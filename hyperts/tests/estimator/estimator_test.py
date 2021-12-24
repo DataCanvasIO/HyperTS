2e29e6e49d4784859853249f01accbd0552d2361
newbei <hegang5862@qq.com>
2021-12-24 10:30:27 +0800

    Add estimator_test.py & hyperts_test.py

---
+++ hyperts/tests/estimator/estimator_test.py
@@ -0,0 +1,84 @@
+import numpy as np
+import hyperts.utils.toolbox as dp
+from hyperts.utils.metrics import accuracy_score
+from hyperts.framework.wrappers.stats_wrappers import ProphetWrapper, ARIMAWrapper, VARWrapper, TSForestWrapper, KNeighborsWrapper
+from hyperts.datasets import load_random_univariate_forecast_dataset, load_random_multivariate_forecast_dataset, load_arrow_head
+
+
+class Test_Estimator():
+
+    def test_Prophet_wrapper(self):
+        X, y = load_random_univariate_forecast_dataset(return_X_y=True)
+        X_train, X_test, y_train, y_test, = dp.temporal_train_test_split(X, y, test_size=0.2)
+        fit_kwargs = {'timestamp': 'ds'}
+        init_kwargs = {
+            'seasonality_mode': 'multiplicative'
+        }
+        model = ProphetWrapper(fit_kwargs, **init_kwargs)
+        model.fit(X_train, y_train)
+
+        y_pred = model.predict(X_test)
+        assert y_pred.shape[0] == y_test.shape[0]
+
+    def test_ARIMA_wrapper(self):
+        X, y = load_random_univariate_forecast_dataset(return_X_y=True)
+        X_train, X_test, y_train, y_test, = dp.temporal_train_test_split(X, y, test_size=0.2)
+        fit_kwargs = {'timestamp': 'ds'}
+        init_kwargs = {
+            'p': 1,
+            'd': 1,
+            'q': 2,
+            'trend': 'c',
+            'y_scale': np.random.choice(['min_max', 'max_abs', 'none'], size=1)[0]
+
+        }
+        model = ARIMAWrapper(fit_kwargs, **init_kwargs)
+        model.fit(X_train, y_train)
+
+        y_pred = model.predict(X_test)
+        assert y_pred.shape[0] == y_test.shape[0]
+
+    def test_VAR_wrapper(self):
+        X, y = load_random_multivariate_forecast_dataset(return_X_y=True)
+        X_train, X_test, y_train, y_test, = dp.temporal_train_test_split(X, y, test_size=0.2)
+        fit_kwargs = {'timestamp': 'ds'}
+        init_kwargs = {
+            'trend': 'c',
+            'y_scale': np.random.choice(['min_max', 'max_abs', 'none'], size=1)[0]
+        }
+        model = VARWrapper(fit_kwargs, **init_kwargs)
+        model.fit(X_train, y_train)
+
+        y_pred = model.predict(X_test)
+        assert y_pred.shape[0] == y_test.shape[0]
+
+    def test_TSF_wrapper(self):
+        X, y = load_arrow_head(return_X_y=True)
+        X_train, X_test, y_train, y_test = dp.random_train_test_split(X, y)
+        fit_kwargs = {}
+        init_kwargs = {
+            'min_interval': 3,
+            'n_estimators': 50,
+        }
+        model = TSForestWrapper(fit_kwargs=fit_kwargs, **init_kwargs)
+
+        model.fit(X_train, y_train)
+        y_pred = model.predict(X_test)
+        score = accuracy_score(y_test, y_pred)
+        assert score > 0
+
+    def test_KNeighbors_wrapper(self):
+        X, y = load_arrow_head(return_X_y=True)
+        X_train, X_test, y_train, y_test = dp.random_train_test_split(X, y)
+        fit_kwargs = {}
+        init_kwargs = {
+            'n_neighbors': 3,
+            'distance': 'ddtw',
+            'x_scale': np.random.choice(['min_max', 'z_score'], size=1)[0]
+        }
+        model = KNeighborsWrapper(fit_kwargs=fit_kwargs, **init_kwargs)
+
+        model.fit(X_train, y_train)
+        y_pred = model.predict(X_test)
+        score = accuracy_score(y_test, y_pred)
+        assert score > 0
---
+++ hyperts/tests/hyperts/hyperts_test.py
@@ -0,0 +1,44 @@
+from hypernets.core.ops import HyperInput
+from hypernets.core.callbacks import SummaryCallback
+from hypernets.core.search_space import HyperSpace, Choice
+from hypernets.searchers.random_searcher import RandomSearcher
+
+from hyperts.hyper_ts import HyperTS
+from hyperts.utils import consts, toolbox as dp
+from hyperts.datasets import load_random_univariate_forecast_dataset
+from hyperts.framework.wrappers.stats_wrappers import SimpleTSEstimator, ProphetWrapper
+
+class Test_HyperTS():
+
+    @classmethod
+    def search_space_one_trial(cls, timestamp):
+        fit_kwargs = {'timestamp': timestamp}
+        space = HyperSpace()
+        with space.as_default():
+            input = HyperInput(name='input1')
+            SimpleTSEstimator(ProphetWrapper, fit_kwargs=fit_kwargs, seasonality_mode=Choice(['additive', 'multiplicative']))(input)
+            space.set_inputs(input)
+        return space
+
+    def test_hyperts(self):
+        X, y = load_random_univariate_forecast_dataset(return_X_y=True)
+        X_train, X_test, y_train, y_test = dp.temporal_train_test_split(X, y, test_horizon=24)
+
+        task = consts.Task_UNIVARIABLE_FORECAST
+        optimize_direction = consts.OptimizeDirection_MINIMIZE
+        reward_metric = consts.Metric_RMSE
+
+        rs = RandomSearcher(lambda : self.search_space_one_trial(timestamp='ds'), optimize_direction=optimize_direction)
+        ht = HyperTS(rs, reward_metric=reward_metric, task=task, callbacks=[SummaryCallback()])
+
+        ht.search(X_train, y_train, X_test, y_test, max_trials=3)
+        best_trial = ht.get_best_trial()
+
+        estimator = ht.final_train(best_trial.space_sample, X_train, y_train)
+        result = estimator.evaluate(X_test, y_test)
+        assert result[reward_metric] > 0
+
+
+
+
+
