b2560daf8f08748691e7bd9efd14cc5f881186a3
Xiaojing Zhang <80235074+zhangxjohn@users.noreply.github.com>
2022-06-07 15:21:56 +0800

    Migrated from __init__ to helper_fn.

--- hyperts/framework/meta_learning/helper_fn.py
+++ hyperts/framework/meta_learning/helper_fn.py
@@ -1,7 +1,33 @@
 # -*- coding:utf-8 -*-
+
+import numpy as np
 from hypernets.utils import logging
 
 logger = logging.get_logger(__name__)
+
+
+def ptp(X):
+    """
+    Range of values (maximum - minimum) along an axis.
+    """
+    num_metafeatures = X.shape[1]
+    domain = np.zeros((num_metafeatures, 2))
+    for i in range(num_metafeatures):
+        domain[i, 0] = np.min(X[:, i])
+        domain[i, 1] = np.max(X[:, i])
+
+    return domain
+
+
+def normalization(metafeatures):
+    """
+    Normalized meta features.
+    """
+    domain = ptp(np.array(metafeatures))
+    normalize = lambda X: (X - domain[:, 0]) / np.ptp(domain, axis=1)
+    normalized_metafeatures = normalize(metafeatures)
+
+    return normalized_metafeatures
 
 
 def warm_start_sample(space_sample, trial_store):
