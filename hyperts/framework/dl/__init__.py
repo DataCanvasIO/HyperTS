5fe742202236dec42b5f95a35ab2fad7b483e164
Xiaojing Zhang <80235074+zhangxjohn@users.noreply.github.com>
2022-07-18 11:03:34 +0800

    Upgrade to 0.1.3.

--- hyperts/framework/dl/__init__.py
+++ hyperts/framework/dl/__init__.py
@@ -1,2 +1,6 @@
 from ._base import BaseDeepEstimator
-from .models import DeepAR, HybirdRNN, LSTNet, NBeats, InceptionTime
+from .models import DeepAR
+from .models import HybirdRNN
+from .models import LSTNet
+from .models import NBeats
+from .models import InceptionTime
--- hyperts/framework/dl/layers/__init__.py
+++ hyperts/framework/dl/layers/__init__.py
@@ -1,8 +1,21 @@
 from tensorflow.keras.layers import *
 
-from ._layers import MultiColEmbedding, WeightedAttention, FeedForwardAttention, AutoRegressive, \
-    Highway, Time2Vec, RevInstanceNormalization, Identity, Shortcut, InceptionBlock, FactorizedReduce
+from ._layers import MultiColEmbedding
+from ._layers import WeightedAttention
+from ._layers import FeedForwardAttention
+from ._layers import AutoRegressive
+from ._layers import Highway
+from ._layers import Time2Vec
+from ._layers import RevInstanceNormalization
+from ._layers import Identity
+from ._layers import Shortcut
+from ._layers import InceptionBlock
+from ._layers import FactorizedReduce
 
-from ._layers import build_input_head, build_denses, build_embeddings, build_output_tail, rnn_forward
+from ._layers import build_input_head
+from ._layers import build_denses
+from ._layers import build_embeddings
+from ._layers import build_output_tail
+from ._layers import rnn_forward
 
 from ._layers import layers_custom_objects
