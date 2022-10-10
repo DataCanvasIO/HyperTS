d151b79d3b67451ffeccd6f4acf2cb593914b556
Xiaojing Zhang <80235074+zhangxjohn@users.noreply.github.com>
2022-10-10 18:00:56 +0800

    Adaptation anomaly detection task.

--- hyperts/utils/plot.py
+++ hyperts/utils/plot.py
@@ -1,7 +1,6 @@
 import numpy as np
-import pandas as pd
 
-from hyperts.utils import get_tool_box
+from hyperts.utils import get_tool_box, consts
 from hypernets.utils import logging
 logger = logging.get_logger(__name__)
 
@@ -27,7 +26,8 @@ def plot_plotly(forecast,
                 history=None,
                 forecast_interval=None,
                 show_forecast_interval=False,
-                include_history=False):
+                include_history=False,
+                **kwargs):
     """Plots forecast trend curves for the forecst task by plotly.
 
     Notes
@@ -60,6 +60,9 @@ def plot_plotly(forecast,
     ----------
     fig : 'plotly.graph_objects.Figure'.
     """
+    task = kwargs.get('task', consts.Task_FORECAST)
+    anomaly_label = kwargs.get('anomaly_label_col')
+
     tb = get_tool_box(forecast)
 
     if not isinstance(target_col, list):
@@ -75,7 +78,7 @@ def plot_plotly(forecast,
 
     ts_free = False if timestamp_col in tb.columns_values(forecast) else True
 
-    y_forecast =forecast[target_col]
+    y_forecast = forecast[target_col] if task in consts.TASK_LIST_FORECAST else forecast[['anomaly']]
     y_actual = actual[target_col] if actual is not None else None
 
     if history is not None and include_history:
@@ -98,7 +101,41 @@ def plot_plotly(forecast,
     fig = go.Figure()
     plt.set_loglevel('WARNING')
 
-    if forecast_interval is not None and show_forecast_interval:
+    if task in consts.TASK_LIST_DETECTION:
+        outliers = y_forecast.loc[y_forecast['anomaly'] == 1]
+        outlier_index = list(outliers.index)
+        outliers_trace = go.Scatter(
+            x=X_forecast.loc[outlier_index],
+            y=y_actual.values[outlier_index, 0],
+            mode='markers',
+            line=dict(color='rgba(31, 119, 180, 0.7)'),
+            name='Anomaly')
+        fig.add_trace(outliers_trace)
+
+        if anomaly_label is not None:
+            from hyperts.utils.metrics import _infer_pos_label
+            pos_label = _infer_pos_label(actual[anomaly_label])
+            actual_outliers = actual.loc[actual[anomaly_label] == pos_label]
+            actual_outliers_index = list(actual_outliers.index)
+            actual_outliers_trace = plt.scatter(
+                x=X_forecast.loc[actual_outliers_index],
+                y=y_actual.values[actual_outliers_index, 0],
+                mode='markers',
+                line=dict(color='rgba(250, 43, 20, 0.7)'),
+                name='Ground Truth')
+            fig.add_trace(actual_outliers_trace)
+
+        severity_score = forecast[['severity']]*np.mean(y_actual.values[:, var_id])
+        severity_trace = go.Scatter(
+            x=X_forecast,
+            y=severity_score.values[:, 0],
+            mode='lines',
+            line=dict(width=0.5,
+                      color='rgba(31, 119, 180, 0.7)'),
+            name='Severity Score')
+        fig.add_trace(severity_trace)
+
+    if forecast_interval is not None and show_forecast_interval and task in consts.TASK_LIST_FORECAST:
         upper_forecast, lower_forecast = forecast_interval[0], forecast_interval[1]
 
         lower_bound = go.Scatter(
@@ -137,14 +174,15 @@ def plot_plotly(forecast,
         )
         fig.add_trace(actual_trace)
 
-    forecast_trace = go.Scatter(
-        x=X_forecast,
-        y=y_forecast.values[:, var_id],
-        mode='lines',
-        line=dict(color='rgba(31, 119, 180, 0.7)'),
-        name='Forecast'
-    )
-    fig.add_trace(forecast_trace)
+    if task in consts.TASK_LIST_FORECAST:
+        forecast_trace = go.Scatter(
+            x=X_forecast,
+            y=y_forecast.values[:, var_id],
+            mode='lines',
+            line=dict(color='rgba(31, 119, 180, 0.7)'),
+            name='Forecast'
+        )
+        fig.add_trace(forecast_trace)
 
     if history is not None and include_history:
         history_trace = go.Scatter(
@@ -185,9 +223,14 @@ def plot_plotly(forecast,
         )
         fig.update_layout(new_layout)
 
+    if task in consts.TASK_LIST_FORECAST:
+        ylabel = y_forecast.columns[var_id]
+    else:
+        ylabel = y_actual.columns[var_id]
+
     layout = go.Layout(
         xaxis=dict(title='Date'),
-        yaxis=dict(title=y_forecast.columns[var_id]),
+        yaxis=dict(title=ylabel),
         title='Actual vs Forecast' if actual is not None else 'Forecast Curve',
         title_x=0.5,
         showlegend=True,
@@ -212,7 +255,8 @@ def plot_mpl(forecast,
              show_forecast_interval=False,
              include_history=False,
              figsize=None,
-             grid=True):
+             grid=True,
+             **kwargs):
     """Plots forecast trend curves for the forecst task by matplotlib.
 
     Notes
@@ -249,6 +293,9 @@ def plot_mpl(forecast,
     ----------
     fig : 'matpltlib..pyplot.figure'.
     """
+    task = kwargs.get('task', consts.Task_FORECAST)
+    anomaly_label = kwargs.get('anomaly_label_col')
+
     tb = get_tool_box(forecast)
 
     if not isinstance(target_col, list):
@@ -264,7 +311,7 @@ def plot_mpl(forecast,
 
     ts_free = False if timestamp_col in tb.columns_values(forecast) else True
 
-    y_forecast =forecast[target_col]
+    y_forecast = forecast[target_col] if task in consts.TASK_LIST_FORECAST else forecast[['anomaly']]
     y_actual = actual[target_col] if actual is not None else None
 
     if history is not None and include_history:
@@ -290,10 +337,22 @@ def plot_mpl(forecast,
                  c='#808080',
                  label='Historical')
 
-    plt.plot(X_forecast,
-             y_forecast.values[:, var_id],
-             c='#1E90FF',
-             label='Forecast')
+        plt.axvline(X_train.iloc[-1],
+                   c='#808080',
+                   alpha=0.5)
+
+        plt.text(X_train.iloc[-1],
+                 np.max(y_train.values[:, var_id]),
+                 s='Observed End Date',
+                 fontsize=12,
+                 horizontalalignment='right',
+                 c='#808080')
+
+    if task in consts.TASK_LIST_FORECAST:
+        plt.plot(X_forecast,
+                 y_forecast.values[:, var_id],
+                 c='#1E90FF',
+                 label='Forecast')
 
     if actual is not None:
         plt.plot(X_forecast,
@@ -302,7 +361,7 @@ def plot_mpl(forecast,
                  alpha=0.5,
                  label='Actual')
 
-    if forecast_interval is not None and show_forecast_interval:
+    if forecast_interval is not None and show_forecast_interval and task in consts.TASK_LIST_FORECAST:
         upper_forecast, lower_forecast = forecast_interval[0], forecast_interval[1]
 
         plt.fill_between(X_forecast,
@@ -312,8 +371,38 @@ def plot_mpl(forecast,
                          alpha=0.5,
                          label='Uncertainty Interval')
 
+    if task in consts.TASK_LIST_DETECTION:
+        outliers = y_forecast.loc[y_forecast['anomaly'] == 1]
+        outlier_index = list(outliers.index)
+        plt.scatter(X_forecast.loc[outlier_index],
+                    y_actual.values[outlier_index, var_id],
+                    c='#1E90FF',
+                    label='Anomaly')
+
+        if anomaly_label is not None:
+            from hyperts.utils.metrics import _infer_pos_label
+            pos_label = _infer_pos_label(actual[anomaly_label])
+            actual_outliers = actual.loc[actual[anomaly_label] == pos_label]
+            actual_outliers_index = list(actual_outliers.index)
+            plt.scatter(X_forecast.loc[actual_outliers_index],
+                        y_actual.values[actual_outliers_index, var_id],
+                        c='#FF0000',
+                        label='Ground Truth')
+
+        severity_score = forecast[['severity']]*np.mean(y_actual.values[:, var_id])
+        plt.plot(
+            X_forecast,
+            severity_score.values[:, 0],
+            c='#1E90FF',
+            alpha=0.5,
+            linewidth=0.2,
+            label='Severity Score')
+
     plt.title('Actual vs Forecast' if actual is not None else 'Forecast Curve', fontsize=16)
-    plt.ylabel(y_forecast.columns[var_id])
+    if task in consts.TASK_LIST_FORECAST:
+        plt.ylabel(y_forecast.columns[var_id])
+    else:
+        plt.ylabel(y_actual.columns[var_id])
     plt.xlabel('Date')
     plt.grid(grid, alpha=0.3)
     plt.legend(fontsize=12, loc=2, bbox_to_anchor=(1.01, 1.0), borderaxespad=0.)
