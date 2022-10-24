dbdc2f7a87a39b3b7741f0313b5a87163e4728a3
Xiaojing Zhang <80235074+zhangxjohn@users.noreply.github.com>
2022-10-24 09:56:28 +0800

    Add anomaly detection dl search space.

--- hyperts/framework/search_space/__init__.py
+++ hyperts/framework/search_space/__init__.py
@@ -3,6 +3,6 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 from ._base import SearchSpaceMixin, HyperParams, WithinColumnSelector
 from .macro_search_space import StatsForecastSearchSpace, StatsClassificationSearchSpace
-from .macro_search_space import DLForecastSearchSpace, DLClassRegressSearchSpace
+from .macro_search_space import DLForecastSearchSpace, DLClassRegressSearchSpace, DLDetectionSearchSpace
 
 from hypernets.core.search_space import Choice, Int, Real, Bool, Constant, Dynamic
