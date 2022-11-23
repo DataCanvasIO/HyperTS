631ed50787306a6f3a98b1624597ed93617fc03c
Xiaojing Zhang <80235074+zhangxjohn@users.noreply.github.com>
2022-11-23 15:35:23 +0800

    Compatible with ``tensorflow==2.11.0``.

--- hyperts/framework/dl/optimizers/__init__.py
+++ hyperts/framework/dl/optimizers/__init__.py
@@ -1,4 +1,11 @@
-from tensorflow.keras.optimizers import *
+import tensorflow as tf
+
+tf_version = str(tf.__version__)
+
+if int(tf_version.split(".")[1]) < 10:
+    from tensorflow.keras.optimizers import *
+else:
+    from tensorflow.keras.optimizers.legacy import *
 
 from ._optimizers import AdamP
 
