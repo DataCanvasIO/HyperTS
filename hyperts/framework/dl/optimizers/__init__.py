b0f1290ce1ba034f739f17f1df765f01d28fdd01
Xiaojing Zhang <80235074+zhangxjohn@users.noreply.github.com>
2022-11-23 15:42:53 +0800

    (#71) Compatible with ``tensorflow==2.11.0``.

--- hyperts/framework/dl/optimizers/__init__.py
+++ hyperts/framework/dl/optimizers/__init__.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 
 tf_version = str(tf.__version__)
 
-if int(tf_version.split(".")[1]) < 10:
+if int(tf_version.split(".")[1]) < 11:
     from tensorflow.keras.optimizers import *
 else:
     from tensorflow.keras.optimizers.legacy import *
